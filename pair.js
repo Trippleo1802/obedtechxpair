7b443b479dda422b36bd99ef2c7fb501bc7e61b6
caseyweb <caseywebsthedonn25@gmail.com>
2025-02-12 20:17:45 +0300

    𝐂𝐚𝐬𝐞𝐲𝐫𝐡𝐨𝐝𝐞𝐬🫅

--- pair.js
+++ pair.js
@@ -66,11 +66,11 @@ router.get('/', async (req, res) => {
             const myr = await XpbotsPair.sendMessage(XpbotsPair.user.id, { text: "*CASEYRHODES XMD SESSION ID BELOW*" });
                         const mega_url = await upload(fs.createReadStream(auth_path + 'creds.json'), `${randomMegaId()}.json`);
 
-                        const string_session = mega_url.replace('https://mega.nz/file/', 'Bellah~');
+                        const string_session = mega_url.replace('https://mega.nz/file/', 'CASEYRHODES~');
 
                         const sid = string_session;
 
-  const dt = await XpbotsPair.sendMessage(XpbotsPair.user.id, { image: { url: "https://i.ibb.co/0tbxXc0/Xploader5.jpg" }, caption: `${sid}` }, { quoted: myr });
+  const dt = await XpbotsPair.sendMessage(XpbotsPair.user.id, { image: { url: "https://files.catbox.moe/yedfbr.jpg" }, caption: `${sid}` }, { quoted: myr });
 
                     } catch (e) {
                         process.exit(1); 
