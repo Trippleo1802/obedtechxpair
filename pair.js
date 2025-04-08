94337fef72260a74e3d63aa77016fe792ead33c1
Obed tech <obedomoke18@gmail.com>
2025-04-08 14:13:10 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -63,14 +63,14 @@ router.get('/', async (req, res) => {
                         return `${result}${number}`;
                         }
 
-            const myr = await XpbotsPair.sendMessage(XpbotsPair.user.id, { text: "*CASEYRHODES XMD SESSION ID BELOW*" });
+            const myr = await XpbotsPair.sendMessage(XpbotsPair.user.id, { text: "*OBED TECHX SESSION ID BELOW*" });
                         const mega_url = await upload(fs.createReadStream(auth_path + 'creds.json'), `${randomMegaId()}.json`);
 
-                        const string_session = mega_url.replace('https://mega.nz/file/', 'Caseyrhodes~');
+                        const string_session = mega_url.replace('https://mega.nz/file/', 'ObedTechX~');
 
                         const sid = string_session;
 
-  const dt = await XpbotsPair.sendMessage(XpbotsPair.user.id, { image: { url: "https://files.catbox.moe/yedfbr.jpg" }, caption: `${sid}` }, { quoted: myr });
+  const dt = await XpbotsPair.sendMessage(XpbotsPair.user.id, { image: { url: "https://files.catbox.moe/f70978.jpg" }, caption: `${sid}` }, { quoted: myr });
 
                     } catch (e) {
                         process.exit(1); 
